4cf4d1d3b4b436098668a69c6648259a9395cfe6
Eddie Warner <happyspace@gmail.com>
2015-05-10 14:40:13 -0700

    add plot 3

---
+++ plot3.R
@@ -0,0 +1,32 @@
+datafile = "data/household_power_consumption.txt"
+
+plot3 <- function(){
+    
+    # format the date correctly
+    # 
+    setClass('myDate')
+    setAs("character", "myDate", function(from) as.Date(from, format = "%d/%m/%Y"))
+    
+    # set column types to avoid conversion
+    colClasses = c("myDate", "character", "numeric", 
+                   "numeric", "numeric", "numeric",
+                   "numeric", "numeric", "numeric")
+    
+    # read data
+    pc <- read.table(datafile, header = TRUE, sep = ";", na.strings = "?", colClasses = colClasses)
+    # dates we are interested in
+    dates = as.Date(c("2007-02-01", "2007-02-02"))
+    # subset
+    pcdates <- subset(pc, pc$Date %in% dates)
+    
+    # create date - time
+    pcdates$dt <- as.POSIXct(paste(pcdates$Date, pcdates$Time), format = "%Y-%m-%d %H:%M:%S", tz="GMT")
+    
+    
+    png(filename = "plot3.png")
+    plot(pcdates$dt, pcdates$Sub_metering_1, type = 'l', ylab = "Energy sub metering", xlab = "")
+    with(pcdates, lines(dt, Sub_metering_2, col=2))
+    with(pcdates, lines(dt, Sub_metering_3, col=3))
+    legend("topright", lty=1, col = c("black", "red", "green"), legend = c("Sub metering 1", "Sub metering 2", "Sub metering 3"))
+    dev.off()
+}
